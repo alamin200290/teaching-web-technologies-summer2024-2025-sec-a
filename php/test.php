5ce0c3a33807779622ff6d476949d84678dc468c
Md.Al-Amin <amin.se.aiub@gmail.com>
2025-08-19 15:51:36 +0600

    Add files via upload

---
+++ php/test.php
@@ -0,0 +1,55 @@
+<?php
+
+     $name = "alamin";
+    // $age = 12;
+    // $cgpa = 3.5;
+
+    // $std = array(1, 'alamin', 3.4);
+    // $std[1];
+    //$std = [1, 'alamin', 3.4];
+    // $stds = [
+    //     [1, 'alamin', 3.4],
+    //     [2, 'xyz', 3.2],
+    //     [3, 'abc', 3.3]
+    // ];
+    // $std[1][2];
+     $std = ['id'=>1, 'name'=>'alamin', 'cgpa'=>2.3];
+    // $std['name'];
+    // $stds = [
+    //     ['id'=>1, 'name'=>'alamin', 'cgpa'=>2.3],
+    //     ['id'=>2, 'name'=>'xyz', 'cgpa'=>2.5],
+    //     ['id'=>3, 'name'=>'abc', 'cgpa'=>3.3]
+    // ];
+    // $stds[2]['cgpa'];
+    // $stds = [
+    //     's1'=>['id'=>1, 'name'=>'alamin', 'cgpa'=>2.3],
+    //     's2'=>['id'=>2, 'name'=>'xyz', 'cgpa'=>2.5],
+    //     's3'=>['id'=>3, 'name'=>'abc', 'cgpa'=>3.3]
+    // ];
+    // $stds['s3']['name'];
+
+    // for($i=0; $i<10; $i++){
+    //     echo "<h1>alamin</h1>";
+    // }
+
+    // foreach($std as $v){
+    //     $v;
+    // }
+
+    // if(10==='10'){
+
+    // }else{
+
+    // }
+
+    // function abc($a=0, $b=0){
+    //     return $a+$b;
+    // }
+
+    // $c = abc();
+
+    //echo "<h1>TEST"." another {$name} test </h1>";
+    //print('test from print');
+    //print_r($std);
+    var_dump($std);
+?>
